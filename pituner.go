24e9700909e1d82b6545ae3917d1d5385038e923
Jacob Budin <self@jacobbudin.com>
2015-07-05 16:08:19 -0400

    Adding parseStations function, related functionality

--- pituner.go
+++ pituner.go
@@ -1,27 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
-type display struct {
-	primary   string
-	secondary string
+type Display struct {
+	Primary   string
+	Secondary string
 }
 
-type station struct {
-	name    string
-	url     string
-	channel int
+type Station struct {
+	Name    string `json:"name"`
+	Url     string `json:"url"`
+	Channel int    `json:"channel"`
 }
 
 func main() {
 	// Parse flags
 	debug := flag.Bool("debug", false, "enable debugging mode")
 	help := flag.Bool("help", false, "show this help")
-	stations := flag.String("stations", "~/.pituner/stations.json", "load station file")
+	stations_file_path := flag.String("stations", "etc/stations.json", "load station file")
 
 	flag.Parse()
 
@@ -30,10 +32,37 @@ func main() {
 		os.Exit(0)
 	}
 
-	fmt.Printf("%t %t %s\n", *debug, *help, *stations)
+	// Load station info
+	stations, err := parseStations(*stations_file_path)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if *debug {
+		fmt.Printf("Loaded %d stations\n", len(stations))
+	}
 }
 
 func showHelp() {
 	fmt.Fprintf(os.Stderr, "Usage of pituner:\n")
 	flag.PrintDefaults()
+}
+
+func parseStations(stations_file_path string) ([]Station, error) {
+	// Open file
+	stations_data, err := ioutil.ReadFile(stations_file_path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Create station data
+	var stations []Station
+
+	if err := json.Unmarshal(stations_data, &stations); err != nil {
+		return nil, err
+	}
+
+	return stations, nil
 }
