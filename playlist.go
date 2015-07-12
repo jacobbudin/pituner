d787694b5f56b218835b003c25c6fa2c77979380
Jacob Budin <self@jacobbudin.com>
2015-07-12 12:08:58 -0400

    Adding more robustness to

--- playlist.go
+++ playlist.go
@@ -9,18 +9,26 @@ import (
 // parsePlaylist downloads a `.pls` file and returns an array
 // of URL `strings` that contain the actual playback URLs
 func parsePlaylist(playlist_url string) []string {
+	urls := make([]string, 0)
 	resp, err := http.Get(playlist_url)
 
 	if err != nil {
 		if DEBUG {
-			fmt.Println("Playlist %s could not be reached", playlist_url)
+			fmt.Println("Playlist %s could not be reached: %s", playlist_url, err.Error())
 		}
 
-		return make([]string, 0)
+		return urls
 	}
 
-	urls := make([]string, 0)
-	contents := make([]byte, 1000)
+	if resp.ContentLength == -1 {
+		if DEBUG {
+			fmt.Println("Playlist %s response was malformed", playlist_url)
+		}
+
+		return urls
+	}
+
+	contents := make([]byte, resp.ContentLength)
 
 	resp.Body.Read(contents)
 	resp.Body.Close()
@@ -34,6 +42,12 @@ func parsePlaylist(playlist_url string) []string {
 			if i != -1 {
 				urls = append(urls, line[i+1:])
 			}
+		}
+	}
+
+	if len(urls) == 0 {
+		if DEBUG {
+			fmt.Println("Playlist %s contained no playback URLs", playlist_url)
 		}
 	}
 
