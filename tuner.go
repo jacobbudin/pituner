d75a166b2091e0b0778cf98e278867e3455b9a6b
Jacob Budin <self@jacobbudin.com>
2015-07-14 00:23:11 -0400

    Adding tuner info function

--- tuner.go
+++ tuner.go
@@ -8,6 +8,7 @@ package main
 import "C"
 
 import (
+	"strings"
 	"unsafe"
 )
 
@@ -30,4 +31,31 @@ func (t *Tuner) stop() {
 	C.BASS_ChannelStop((C.DWORD)(t.stream))
 	C.BASS_StreamFree(t.stream)
 	C.free((unsafe.Pointer)(t.stream_url))
+}
+
+// info gets playback info, if playing
+func (t *Tuner) info() string {
+	if t.stream == 0 {
+		return ""
+	}
+
+	info := C.GoString(C.BASS_ChannelGetTags((C.DWORD)(t.stream), C.BASS_TAG_META))
+	trigger := "StreamTitle='"
+
+	if i := strings.Index(info, trigger); i != -1 {
+		b := i + len(trigger)
+		e := len(info)
+
+		// find next unescaped single quote
+		for j, r := range info {
+			if j > b && r == '\'' && info[j-1] != '\\' {
+				e = j
+				break
+			}
+		}
+
+		return info[b:e]
+	}
+
+	return info
 }
